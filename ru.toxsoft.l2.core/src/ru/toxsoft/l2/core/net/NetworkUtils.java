226b1263ce6a28bc0e99cdbb34eed88091a04729
pme <pme@toxsoft.ru>
2022-11-28 18:10:33 +0300

    Подключение сервиса качества

--- ru.toxsoft.l2.core/src/ru/toxsoft/l2/core/net/NetworkUtils.java
+++ ru.toxsoft.l2.core/src/ru/toxsoft/l2/core/net/NetworkUtils.java
@@ -3,10 +3,14 @@ package ru.toxsoft.l2.core.net;
 import org.toxsoft.core.log4j.*;
 import org.toxsoft.core.tslib.coll.*;
 import org.toxsoft.core.tslib.gw.gwid.*;
+import org.toxsoft.core.tslib.utils.errors.*;
 import org.toxsoft.core.tslib.utils.logs.*;
+import org.toxsoft.uskat.concurrent.*;
 import org.toxsoft.uskat.core.*;
 import org.toxsoft.uskat.core.api.rtdserv.*;
 import org.toxsoft.uskat.core.connection.*;
+import org.toxsoft.uskat.dataquality.lib.*;
+import org.toxsoft.uskat.dataquality.s5.concurrent.*;
 
 import ru.toxsoft.l2.core.main.impl.*;
 import ru.toxsoft.l2.core.net.impl.*;
@@ -54,24 +58,28 @@ public class NetworkUtils {
       logger.error( "Data params are not added to quality service due to Connection is not connected" );
       return;
     }
+
+    ISkCoreApi clientApi = aConnection.coreApi();
+    ISkDataQualityService qService = getDataQualityService( clientApi );
+
     // при отсутствии сервиса качества - выйти
-    // if( !aConnection.coreApi().services().hasKey( ISkDataQualityService.SERVICE_ID ) ) {
-    // logger.error( "Data params are not added to quality service due to Quality service is not available" );
-    // return;
-    // }
+    if( qService == null ) {
+
+      // ((S5SynchronizedCoreApi)aConnection.coreApi()).addService();
+
+      logger.error( "Data params are not added to quality service due to Quality service is not available" );
+      return;
+    }
 
     GwidList resources = new GwidList();
     resources.addAll( aCurrDataSet.keys() );
 
-    ISkCoreApi clientApi = aConnection.coreApi();
+    // (ISkDataQualityService)clientApi.services().getByKey( ISkDataQualityService.SERVICE_ID );
 
-    // ISkDataQualityService qService =
-    // (ISkDataQualityService)clientApi.services().get( ISkDataQualityService.SERVICE_ID );
-    //
     // Skid sessionID = aConnection.sessionInfo().skid();
-    //
-    // qService.addConnectedResources( sessionID, resources );
-    logger.info( "IDataQualityService addConnectedResources: size = %d", resources.size() );
+
+    qService.addConnectedResources( resources );
+    logger.info( "IDataQualityService addConnectedResources: size = %d", Integer.valueOf( resources.size() ) );
   }
 
   /**
@@ -87,24 +95,35 @@ public class NetworkUtils {
       logger.error( "Data params are not removed from quality service due to Connection is not connected" );
       return;
     }
+
+    ISkCoreApi clientApi = aConnection.coreApi();
+
+    ISkDataQualityService qService = getDataQualityService( clientApi );
+
     // при отсутствии сервиса качества - выйти
-    // if( !aConnection.coreApi().services().hasKey( ISkDataQualityService.SERVICE_ID ) ) {
-    // logger.error( "Data params are not removed from quality service due to Quality service is not available" );
-    // return;
-    // }
+    if( qService == null ) {
+      logger.error( "Data params are not removed from quality service due to Quality service is not available" );
+      return;
+    }
 
     GwidList resources = new GwidList();
     resources.addAll( aCurrDataSet.keys() );
 
-    ISkCoreApi clientApi = aConnection.coreApi();
+    // (ISkDataQualityService)clientApi.services().getByKey( ISkDataQualityService.SERVICE_ID );
 
-    // ISkDataQualityService qService =
-    // (ISkDataQualityService)clientApi.services().get( ISkDataQualityService.SERVICE_ID );
-    //
-    // Skid sessionID = aConnection.sessionInfo().skid();
-    //
-    // qService.removeConnectedResources( sessionID, resources );
-    logger.info( "IDataQualityService removeDataFromQualityService: size = %d", resources.size() );
+    qService.removeConnectedResources( resources );
+    logger.info( "IDataQualityService removeDataFromQualityService: size = %d", Integer.valueOf( resources.size() ) );
+  }
+
+  private static ISkDataQualityService getDataQualityService( ISkCoreApi aCoreApi ) {
+    TsNullArgumentRtException.checkNull( aCoreApi );
+    ISkDataQualityService service =
+        (ISkDataQualityService)aCoreApi.services().findByKey( ISkDataQualityService.SERVICE_ID );
+    if( service == null ) {
+      service = new S5SynchronizedDataQualityService( (S5SynchronizedCoreApi)aCoreApi );
+      ((S5SynchronizedCoreApi)aCoreApi).services().put( ISkDataQualityService.SERVICE_ID, service );
+    }
+    return service;
   }
 
 }
