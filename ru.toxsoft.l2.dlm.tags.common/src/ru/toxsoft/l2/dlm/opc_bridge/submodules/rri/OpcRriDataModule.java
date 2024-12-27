c139d6e32f555ec986cdd6fedd465c33080fa8d8
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2024-12-27 08:31:15 +0300

    ignore RRI module if no config info

--- ru.toxsoft.l2.dlm.tags.common/src/ru/toxsoft/l2/dlm/opc_bridge/submodules/rri/OpcRriDataModule.java
+++ ru.toxsoft.l2.dlm.tags.common/src/ru/toxsoft/l2/dlm/opc_bridge/submodules/rri/OpcRriDataModule.java
@@ -111,24 +111,26 @@ public class OpcRriDataModule
       throws TsIllegalArgumentRtException {
 
     IAvTree rriDefs = aConfig.params().nodes().findByKey( RRI_DEFS );
-    // читаем описание конфигурации самого модуля
-    statusRriMonitor.config( rriDefs );
-    // далее имеет смысл работать только если
-    if( statusRriMonitor.isConfigured() ) {
-      IAvTree rriNodes = rriDefs.nodes().findByKey( RRI_NODES );
+    if( rriDefs.arrayLength() > 0 ) {
+      // читаем описание конфигурации самого модуля
+      statusRriMonitor.config( rriDefs );
+      // далее имеет смысл работать только если
+      if( statusRriMonitor.isConfigured() ) {
+        IAvTree rriNodes = rriDefs.nodes().findByKey( RRI_NODES );
 
-      // наполнение конфигуратора данными (для данных НСИ)
-      if( rriNodes != null && rriNodes.isArray() ) {
-        for( int i = 0; i < rriNodes.arrayLength(); i++ ) {
-          // описание одного НСИ даннного
-          IAvTree oneRriAttrDef = rriNodes.arrayElement( i );
+        // наполнение конфигуратора данными (для данных НСИ)
+        if( rriNodes != null && rriNodes.isArray() ) {
+          for( int i = 0; i < rriNodes.arrayLength(); i++ ) {
+            // описание одного НСИ даннного
+            IAvTree oneRriAttrDef = rriNodes.arrayElement( i );
 
-          initializer.addDataConfigParamsForTransmitter( oneRriAttrDef, complexTagsContainer );
+            initializer.addDataConfigParamsForTransmitter( oneRriAttrDef, complexTagsContainer );
+          }
         }
+        // создание по конфигурации описаний для регистрации в сервисе
+        IAvTree cmdClassDefs = aConfig.params().nodes().findByKey( RRI_CMD_CLASS_DEFS );
+        commandsDefByObjNames = CommandsModule.createCmdDefs( cmdClassDefs );
       }
-      // создание по конфигурации описаний для регистрации в сервисе
-      IAvTree cmdClassDefs = aConfig.params().nodes().findByKey( RRI_CMD_CLASS_DEFS );
-      commandsDefByObjNames = CommandsModule.createCmdDefs( cmdClassDefs );
     }
   }
 
