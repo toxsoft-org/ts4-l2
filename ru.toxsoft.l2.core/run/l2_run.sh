14ca765b72355f66bc2ef2ac57203225d4f21029
ts <ts@VDM-NOTEBOOK>
2026-08-10 20:14:30 +0300

    run/l2_run.sh: timzone updated

--- ru.toxsoft.l2.core/run/l2_run.sh
+++ ru.toxsoft.l2.core/run/l2_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
-export JAVA_HOME=/usr/lib/jvm/jdk-21-oracle-x64
+# export JAVA_HOME=/usr/lib/jvm/jdk-21-oracle-x64
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 
 # run configuration ID
 export USKAT_L2CORE_RUN_ID=run
@@ -12,7 +12,7 @@ export USKAT_MEMORY=1024M
 export USKAT_COUNTRY=RU
 export USKAT_LANG=ru
 # timezone
-export USKAT_TZ=Asia/Irkutsk
+export USKAT_TZ=Europe/Moscow
 
 _JVM_OPS_MEMORY="-Xms${USKAT_MEMORY} -Xmx${USKAT_MEMORY} -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m"
 _JVM_OPS_DEBUG="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8790"
